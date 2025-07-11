afc3fdd863edd456b85c0beea15f3dc469e144b3
agenthummingbird <jjtong2006@gmail.com>
2025-07-11 16:10:42 -0500

    Reverted Demo back to Hello, World

--- Break Out Break Down Official/ContentView.swift
+++ Break Out Break Down Official/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Demo")
+            Text("Hello, World")
         }
         .padding()
     }
