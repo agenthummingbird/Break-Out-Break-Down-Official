9f179b9a484276667549c42e4c939f631f8809d6
agenthummingbird <jjtong2006@gmail.com>
2025-07-11 22:05:24 -0500

    Created drop down menu for information page. also added examplePhoto to Assets
    Things to add:
    - convert the string for sourceURL to a link to be clicked on
    - change the name "DiseaseInfoBlock" if it is confusing to use
    - possibly optimize this if I wrote it inefficiently

--- Break Out Break Down Official/infoTab.swift
+++ Break Out Break Down Official/infoTab.swift
@@ -9,10 +9,57 @@ import SwiftUI
 
 struct infoTab: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Skin Condition Information:")
+            
+            //Example Drop Down Texts
+            DiseaseInfoBlock(name: "Eczema", info: "Itchy and dry spots on the skin which ...", sourceURL: "example.com", imageName: "examplePhoto")
+            DiseaseInfoBlock(name: "Rosacea", info: "Pink patches on the skin...", sourceURL: "example.com", imageName: "examplePhoto")
+            DiseaseInfoBlock(name: "Blackheads", info: "Oxidized black pimples on the skin...", sourceURL: "example.com", imageName: "examplePhoto")
+        }
     }
 }
 
 #Preview {
     infoTab()
+}
+
+// Creates a reusable drop down tab that can be copy and pasted in a VStack to construct a information page
+
+struct DiseaseInfoBlock: View {
+    //initializes constant properties that are defined by parameters
+    let name: String
+    let info: String
+    let sourceURL: String
+    let imageName: String
+    
+    var body: some View{
+        
+        // DisclosureGroup is the drop down tab where the name of the condition (name) is its parameter and its contents is the information we want to drop to (the V and Hstacks using the info, imageName, and sourceURL)
+        DisclosureGroup(name){
+            VStack{
+                HStack{
+                    Image(imageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 150, height: 90, alignment: .leading)
+                    Spacer()
+                    Text("\(info)")
+                        .background(Color.teal)
+                        .frame(width: 200, alignment: .leading)
+                    
+                }
+                Text("Learn More: \(sourceURL)")
+                    .font(.system(size:10))
+                    .frame(alignment: .center)
+                    .padding([.top, .bottom], 5)
+
+            }
+        }
+        .padding(.horizontal, 20)
+    }
+    
+    
+    
+    
 }
